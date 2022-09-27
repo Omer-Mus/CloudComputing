1685900f4cca57a77a0844c83e6123b2ad8df12a
omer mustel <omustel@gmail.com>
2022-09-27 10:58:19 -0400

    microservice

---
+++ F22-Starter-Microservice/src/columbia_student_resource.py
@@ -0,0 +1,40 @@
+import pymysql
+
+import os
+
+
+class ColumbiaStudentResource:
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def _get_connection():
+
+        # usr = os.environ.get("DBUSER")
+        # pw = os.environ.get("DBPW")
+        # h = os.environ.get("DBHOST")
+        usr = "root"
+        pw = "Revamped1!"
+        h = 'localhost'
+
+        conn = pymysql.connect(
+            user=usr,
+            password=pw,
+            host=h,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
+
+    @staticmethod
+    def get_by_key(key):
+
+        sql = "SELECT * FROM new_db.columbia_student where uni=%s";
+        conn = ColumbiaStudentResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=key)
+        result = cur.fetchone()
+
+        return result
+
